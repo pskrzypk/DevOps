36b4f32464e45b750d46e9eb7d34e97bc9095145
Pawel <pawel-skrzypkowski@outlook.com>
2021-03-21 17:27:41 +0100

    lab3

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.get('/hello', (req, res) => {
+app.get('/api', (req, res) => {
     res.send("Hello World from express server");
+});
+
+app.post('/api', (req, res) => {
+    var dywidenda = req.query['dywidenda'];
+
+    res.send("Zapisana dywidenda: " + dywidenda);
 });
 
 const PORT = 9090;
